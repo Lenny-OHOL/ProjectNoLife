f7a801db3821d4aebf26808f59ef3e8fcd5b7012
lenny <newyearpioneer@gmail.com>
2025-11-18 22:58:04 -0800

    Cleaned Updated repo

--- getAllObjectNameWords.sh
+++
@@ -1,5 +0,0 @@
-for file in objects/*.txt; do
-  if [ -f "$file" ]; then
-    head -n 2 "$file" | tail -n 1| sed "s/#.*//g" | sed "s/$.*//g" | sed "s/@.*//g" | sed "s/ /\n/g" | sed "s/-/\n/g"
-  fi
-done | grep -v "0" |  grep -v "," |  grep -v "\." |awk 'NF' | sed '/^.$/d' | tr '[:upper:]' '[:lower:]' | sort | uniq
